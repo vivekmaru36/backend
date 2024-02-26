5ff850ded34b131601f1c91464adb711c8e89396
vivekmaru36 <vivekmaru036@gmail.com>
2024-02-26 19:18:00 +0530

    Lec Adding done

--- Rfid_backend/controllers/HardwareController.js
+++ Rfid_backend/controllers/HardwareController.js
@@ -34,7 +34,7 @@ const Hardware = require("../models/Hardware");
 // });
 
 const setLec = asyncHandler(async (req, res) => {
-    const { Teacher, Subject, stime, etime, date, rfidno , course } = req.body;
+    const { Teacher, Subject, stime, etime, date, rfidno, course } = req.body;
 
     console.log(Teacher);
     console.log(Subject);
@@ -45,7 +45,7 @@ const setLec = asyncHandler(async (req, res) => {
     console.log(course);
 
     // Confirm Data
-    if (!Teacher || !Subject || !stime || !etime || !date || !rfidno ||!course) {
+    if (!Teacher || !Subject || !stime || !etime || !date || !rfidno || !course) {
         return res.status(400).json({ message: "All fields are required" });
     }
 
@@ -67,8 +67,8 @@ const setLec = asyncHandler(async (req, res) => {
     const existingDocument = await Hardware.findOne();
 
     if (existingDocument) {
-      // Document already exists, send an error response
-      return res.status(400).json({ success: false, message: 'A document already exists. Only one document is allowed.' });
+        // Document already exists, send an error response
+        return res.status(400).json({ success: false, message: 'A document already exists. Only one document is allowed.' });
     }
 
     // Create and Store New Lec
@@ -85,22 +85,46 @@ const getLec = asyncHandler(async (req, res) => {
     try {
         // Check if a document already exists
         const hardwaredetails = await Hardware.findOne();
-    
+
         if (!hardwaredetails) {
-          // NO Document exists, send an error response
-          return res.status(404).json({ success: false, message: 'No hardware details available.' });
+            // NO Document exists, send an error response
+            return res.status(404).json({ success: false, message: 'No hardware details available.' });
         }
-    
+
         // Document found, send the details in the response
         res.status(200).json({ success: true, hardwaredetails });
-      } catch (error) {
+    } catch (error) {
         console.error('Error Fetching lec1 details:', error);
         res.status(500).json({ success: false, message: 'Internal Server Error' });
-      }
+    }
+});
+
+
+const forceDeLec = asyncHandler(async (req, res) => {
+    const { rfid } = req.body;
+    try {
+        // Check if the user is a teacher and present in hardware
+        const isTeacher = await Hardware.findOne({ rfidno:rfid });
+        console.log(rfid);
+        console.log(isTeacher);
+
+        if (isTeacher) {
+            // If the user is a teacher, delete all data from the hardware collection
+            await Hardware.deleteMany({ rfidno:rfid });
+
+            return res.status(200).json({ success: true, message: 'Hardware data deleted successfully.' });
+        } else {
+            return res.status(403).json({ success: false, message: 'Access forbidden. Only teachers can delete hardware data.' });
+        }
+    } catch (error) {
+        console.error('Error deleting hardware data:', error);
+        res.status(500).json({ success: false, message: 'Internal Server Error' });
+    }
 });
 
 
 module.exports = {
     setLec,
-    getLec
-  };
+    getLec,
+    forceDeLec
+};
--- Rfid_backend/routes/HardwareRoutes.js
+++ Rfid_backend/routes/HardwareRoutes.js
@@ -6,6 +6,7 @@ const HardwareController=require('../controllers/HardwareController')
 
 router.route('/').post(HardwareController.setLec)
 router.route('/').get(HardwareController.getLec)
+router.route('/').delete(HardwareController.forceDeLec)
 
 
 module.exports = router;
