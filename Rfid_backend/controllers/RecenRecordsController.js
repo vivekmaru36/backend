a5e98d6c79b58a2bcf8f717bf89352519ddc4107
vivekmaru36 <vivekmaru036@gmail.com>
2024-03-13 20:50:21 +0530

    admin recent records

--- Rfid_backend/controllers/RecenRecordsController.js
+++ Rfid_backend/controllers/RecenRecordsController.js
@@ -24,7 +24,7 @@ const RecentRecordsoOnRfid = asyncHandler(async (req, res) => {
         .sort({ currentTime: -1 }) // 1 for ascending order, -1 for descending order
         .lean()
         .exec();
-    
+
     // console.log("Document 2 : ", document2);
 
     const library = await LibraryModel.find({ rfid }, { geoLocation: 0, Ip: 0 })
@@ -55,6 +55,55 @@ const RecentRecordsoOnRfid = asyncHandler(async (req, res) => {
 
 });
 
+const RecentRecordsoOnRfidAdmin = asyncHandler(async (req, res) => {
+    const { rfid } = req.body;
+    console.log(rfid);
+
+    // const document = await HardwareRfidSwipe.find({ rfid }, { geoLocation: 0, Ip: 0 }).lean().exec();
+    const document = await HardwareRfidSwipe.find({}, { rfid: 0, geoLocation: 0, Ip: 0 })
+        .sort({ currentTime: -1 }) // Sorting by currentTime in descending order
+        .lean()
+        .exec();
+
+    // console.log("Document 1 : ", document);
+
+    const document2 = await EntryGateModel.find({}, { rfid: 0, geoLocation: 0, Ip: 0 })
+    .sort({ currentTime: -1 }) // Sorting by currentTime in descending order
+    .lean()
+    .exec();
+
+    // console.log("Document 2 : ", document2);
+
+    const library = await LibraryModel.find({}, { geoLocation: 0, Ip: 0 })
+        .sort({ currentTime: -1 }) // 1 for ascending order, -1 for descending order
+        .lean()
+        .exec();
+
+    // console.log(library)
+
+    const Auditorium = await AuditoriumModel.find({}, { geoLocation: 0, Ip: 0 })
+        .sort({ currentTime: -1 }) // 1 for ascending order, -1 for descending order
+        .lean()
+        .exec();
+    // Combine both document1 and document2 arrays into a single array
+    const document3 = [...document, ...document2, ...library, ...Auditorium]
+
+    // Sort the combined array based on currentTime in descending order
+    document3.sort((a, b) => new Date(b.currentTime) - new Date(a.currentTime));
+    console.log(document3);
+
+
+    if (document3.length > 0) {
+        // If documents are found, send the sorted combined array as the response
+        return res.status(200).json({ document: document3 });
+    } else {
+        // If no documents are found, send a 404 Not Found response
+        return res.status(404).json({ message: "No recent records found for the given RFID." });
+    }
+
+});
+
 module.exports = {
     RecentRecordsoOnRfid,
+    RecentRecordsoOnRfidAdmin
 };
--- Rfid_backend/routes/RecentRecordsRoutes.js
+++ Rfid_backend/routes/RecentRecordsRoutes.js
@@ -4,5 +4,6 @@ const router = express.Router();
 const RecenRecordsController = require("../controllers/RecenRecordsController")
 
 router.route('/').post(RecenRecordsController.RecentRecordsoOnRfid);
+router.route('/AdminRecent').post(RecenRecordsController.RecentRecordsoOnRfidAdmin);
 
 module.exports = router;
